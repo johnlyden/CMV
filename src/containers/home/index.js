d73ded96a8c841fdbe917d123c16685abceca058
johnlyden <john.lyden86@gmail.com>
2018-01-10 18:56:41 -0600

    added home container

---
+++ src/containers/home/index.js
@@ -0,0 +1,19 @@
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
+const Home = props => (
+  <div>
+    <h1>Home</h1>
+    <p>Welcome home!</p>
+    <button onClick={() => props.changePage()}>Go to about page via redux</button>
+  </div>
+)
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+  changePage: () => push('/about-us')
+}, dispatch)
+
+export default connect(
+  null, 
+  mapDispatchToProps
+)(Home)
